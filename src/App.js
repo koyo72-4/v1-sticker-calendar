b9dbc5f911ea7e1ac84eaca3ad94a1c2ec87861c
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2019-11-06 21:23:02 -0500

    indent using tabs

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    const month = [];
-    for (let i = 1; i < 30; i += 7) {
-      const week = [];
-      for (let j = i; j < i + 7 && j < 31; j++) {
-        week.push(j);
-      }
-      month.push(week);
-    }
+	render() {
+		const month = [];
+		for (let i = 1; i < 30; i += 7) {
+			const week = [];
+		for (let j = i; j < i + 7 && j < 31; j++) {
+			week.push(j);
+		}
+		month.push(week);
+	}
 
-    return (
-      <div>
-        <h1>Sticker Calendar</h1>
-        <p>What is your goal?</p>
-        <select>
-          <option value="exercise">Exercise</option>
-          <option value="novel">Work on your novel</option>
-          <option value="instrument">Practice a musical instrument</option>
-          <option value="sweets">Avoid sweets</option>
-        </select>
-        <br/>
-        <br/>
-        <table style={{tableLayout: 'fixed'}}>
-          <caption>April 2019</caption>
-          <thead>
-            <tr>
-              <th>Sunday</th>
-              <th>Monday</th>
-              <th>Tuesday</th>
-              <th>Wednesday</th>
-              <th>Thursday</th>
-              <th>Friday</th>
-              <th>Saturday</th>
-            </tr>
-          </thead>
-          <tbody>
-            {month.map(week => {
-              return (
-                <tr>{week.map(day => <td>{day}</td>)}</tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<h1>Sticker Calendar</h1>
+				<p>What is your goal?</p>
+				<select>
+					<option value="exercise">Exercise</option>
+					<option value="novel">Work on your novel</option>
+					<option value="instrument">Practice a musical instrument</option>
+					<option value="sweets">Avoid sweets</option>
+				</select>
+				<br/>
+				<br/>
+				<table style={{tableLayout: 'fixed'}}>
+					<caption>April 2019</caption>
+					<thead>
+						<tr>
+							<th>Sunday</th>
+							<th>Monday</th>
+							<th>Tuesday</th>
+							<th>Wednesday</th>
+							<th>Thursday</th>
+							<th>Friday</th>
+							<th>Saturday</th>
+						</tr>
+					</thead>
+					<tbody>
+						{month.map(week => {
+							return (
+								<tr>{week.map(day => <td>{day}</td>)}</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default App;
